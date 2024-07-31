2b86de47b9d8790e83e10dd558056f9a143acf27
Grace <gracecossai@gmail.com>
2024-07-31 08:25:46 +0100

    Form completed

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
   // This will make our price formatted to 2 decimal places.
@@ -14,7 +15,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <></>
+        <MealItemForm />
       </div>
     </li>
   );
